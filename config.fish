42dbaf895af4884bbe92683b3d277636957703b7
Nikita Blizniuk <nikita.bliznyuk@tensquaregames.com>
2022-06-25 11:22:46 +0200

    initial empty commit from work account

--- config.fish
+++ config.fish
@@ -1,8 +1,15 @@
 if status is-interactive
-  alias ls="exa"
-  alias ll="exa -l"
-  alias la="exa -la"
-  alias lt="exa --tree"
+    # Commands to run in interactive sessions can go here
 end
+
+alias python=python3
+alias pip=pip3
+
+alias ls="exa"
+alias ll="exa -l"
+alias la="exa -la"
+
+thefuck --alias | source
+zoxide init fish | source
 
 starship init fish | source
